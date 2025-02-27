f299d157173b0d662f69e8800dd092ae344d80fc
Pouria Rezaei <rezaeizadehpouria@gmail.com>
2025-02-26 21:58:05 -0800

    Quick fix

--- backend/dist/config/db.js
+++
@@ -1,8 +0,0 @@
-// PGHOST='ep-bold-cherry-a5hslwdj-pooler.us-east-2.aws.neon.tech'
-// PGDATABASE='neondb'
-// PGUSER='neondb_owner'
-// PGPASSWORD='npg_fMPD3q2NuEkB'
-// PGPORT=5432
-import dotenv from "dotenv";
-dotenv.config();
-export const host = process.env.PGHOST;
